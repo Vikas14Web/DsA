c5dce18a94a288f10fee705b7a50691055ad5747
Vikas <vikaschouhan14@gmail.com>
2022-09-07 23:08:25 +0530

    Update Stack Permutations.java
    Easy to understand and simple logic

--- Stack Permutations.java
+++ Stack Permutations.java
@@ -1,20 +1,24 @@
 class Solution {
     public static int isStackPermutation(int n, int[] ip, int[] op) {
         // code here
-        Stack<Integer> s=new Stack<>();
-        int i=0;
-        int j=0;
-        while(j<n){
-            while(i<n && (s.isEmpty() || s.peek()!=op[j])){
-                s.push(ip[i]);
-                i++;
-            }
-            while(!s.isEmpty() && s.peek()==op[j]){
-                s.pop();
+        Stack<Integer> stack = new Stack<>();
+        int i=0,j=0;
+        while(i<n){
+            // Add the element from the ip array ====
+            stack.push(ip[i]);
+            // Check if the stack top contains op element =====
+            if(stack.peek() == op[j]){
+                stack.pop(); // Remove element and increment op pointer ====
                 j++;
+                // Check remaining elemtns in stack for current op element =======
+                while(!stack.isEmpty() && stack.peek() == op[j]){
+                    stack.pop();
+                    j++;
+                }
             }
-            if(i==n && !s.isEmpty() && s.peek()!=op[j])return 0;
+            i++;
         }
-        return 1;
+        // Return true if stack is empty ========
+        return stack.isEmpty()?1:0;
     }
 }
